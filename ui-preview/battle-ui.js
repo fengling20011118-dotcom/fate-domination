5ef03709cd8c0c5afaa8d72257b84ca16bb31ddf
fenglingyue <fengling20011118@gmail.com>
2026-09-23 13:37:38 +0800

    feat: add character skill hover previews

--- ui-preview/battle-ui.js
+++ ui-preview/battle-ui.js
@@ -475,7 +475,7 @@ function render(host,options={}){
       cancelPreviewDelay();previewHoverTarget=target;previewPointer={clientX:event.clientX,clientY:event.clientY};
       if(target.matches('.hand .card')){
         preview.classList.remove('show');
-        previewDelayTimer=window.setTimeout(()=>{if(previewHoverTarget===target&&target.matches(':hover'))renderCardPreview(target,previewPointer)},1500);
+        previewDelayTimer=window.setTimeout(()=>{if(previewHoverTarget===target&&target.matches(':hover'))renderCardPreview(target,previewPointer)},1000);
       }else renderCardPreview(target,event);
     });
     root.addEventListener('pointermove',event=>{
